9bcd78cae68500c5526c2181f3aa45eb31e39eef
naftali spitz <0012ab@gmail.com>
2025-07-07 17:10:13 +0300

    Add security configuration, update application properties, and enhance dashboard and login pages

---
+++ src/main/java/com/naftalibank/bank/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.naftalibank.bank.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        
+        return http
+            .csrf(csrf -> csrf.ignoringRequestMatchers(request -> request.getServletPath().startsWith("/h2-console")))
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/h2-console/**").permitAll()
+                .requestMatchers("/account-holder/**").permitAll()
+                .requestMatchers("/account/**").permitAll()
+                .requestMatchers("/transaction/**").permitAll()
+            )
+            .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin()))
+            .build();
+    }
+}
